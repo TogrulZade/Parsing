658c61f392f10f7a3f98c942eb73143c2a6274b8
j <jk>
2023-09-13 10:53:43 +0400

    Parsing

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,50 +5,93 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    {{-- <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"> --}}
     <link rel="stylesheet" href="{{asset('/css/style.css')}}">
-    <title>Parsing</title>
+
+    <title>Avromed Tool</title>
 </head>
 <body style="background-color: #f1f5f8;">
-    <nav class="navbar navbar-expand-sm navbar-dark bg-primary mb-3">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-success mb-3">
         <div class="container">
-            <a class="navbar-brand" href="/">Demo Parsing System</a>
+            <a class="navbar-brand" href="/">Parsing System</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                 aria-expanded="false" aria-label="Toggle navigation"></button>
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#" aria-current="page">Ana Səhifə <span class="visually-hidden">(current)</span></a>
+                        <a class="nav-link active" href="/" aria-current="page">Ana Səhifə <span class="visually-hidden">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Bütün məhsullar</a>
+                        <a class="nav-link active" href="#">Bütün dərmanlar</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Endirimlər</a>
+                        <a class="active nav-link" href="/settings">Settings</a>
                     </li>
-                    <li class="nav-item dropdown">
+                    {{-- <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                         <div class="dropdown-menu" aria-labelledby="dropdownId">
                             <a class="dropdown-item" href="#">Action 1</a>
                             <a class="dropdown-item" href="#">Action 2</a>
                         </div>
-                    </li>
+                    </li> --}}
                 </ul>
-                <form class="d-flex my-2 my-lg-0">
+                {{-- <form class="d-flex my-2 my-lg-0">
                     <input class="form-control me-sm-2" type="text" placeholder="Search">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                </form> --}}
             </div>
         </div>
     </nav>
     {{-- ./ nav --}}
 <body>
-    <div class="container">
+    <div class="containe-fluid m-3">
         <div class="row">
+            @include("partials.leftmenu")
             @yield('content')
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+
+        <script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+    var data = [
+            [
+                "Tiger Nixon",
+                "System Architect",
+                "Edinburgh",
+                "5421",
+                "2011/04/25",
+                "$3,120"
+            ]
+        ];
+
+        $(document).ready( function () {
+            var clicked_value = '';
+            $('#myTable').DataTable({
+                initComplete: function () {
+                    var clicked = false;
+                    var api = this.api();
+                    api.$('td').click(function () {
+                        if(clicked_value != this.innerHTML){
+                            api.search(this.innerHTML).draw();
+                        }else{
+                            api.search("").draw();
+                        }
+                        clicked_value = this.innerHTML;
+                    });
+
+
+                },
+            });
+            
+            
+        });
+</script>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\KontaktHomeController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\RetailController;
+use App\Http\Controllers\DermanController;
+use App\Http\Controllers\SeleniumController;
 
 
-Route::get('/', [IndexController::class, 'index']);
-Route::get('/bakuelectronics', [IndexController::class, 'getFromCategory']);
+Route::get('/', [SiteController::class, 'show']);
 Route::get('item', [IndexController::class, 'items']);
-Route::get('wt', [IndexController::class, 'wt']);
-
-Route::get('kontakthome', [KontaktHomeController::class, 'index']);
-Route::get('maxi', [IndexController::class, 'maxi']);
 
 Route::get('getFrom', [IndexController::class, 'getFrom']);
 Route::post('getFrom', [IndexController::class, 'getFromAction']);
-
-Route::get('fromKontakt', [KontaktHomeController::class, 'getFrom']);
-Route::post('fromKontakt', [KontaktHomeController::class, 'getFromAction']);
 
 Route::get('automation', [IndexController::class, 'automation']);
 Route::post('automation', [IndexController::class, 'automationAction']);
@@ -33,6 +30,42 @@ Route::get('word', [IndexController::class, 'word']);
 Route::get('turbo', [IndexController::class, 'turbo']);
 
 
-Route::group(['prefix'=>'kontakt'], function(){
+Route::group(['prefix' => 'kontakt'], function () {
     Route::get('detail/{url}', [KontaktHomeController::class, "detail"]);
 });
+
+Route::group(
+    ['prefix' => 'settings'],
+    function () {
+
+        Route::get('/', [SettingController::class, 'index'])->name("settings");
+        Route::get('addsite', [SettingController::class, 'addsite']);
+        Route::post('addsite', [SettingController::class, 'addsiteAction']);
+    }
+);
+
+Route::group(['prefix' => 'site'], function () {
+    Route::get('/', [SiteController::class, "show"])->name("sites");
+    Route::get('getdata/{id}', [SiteController::class, "getdata"]);
+    Route::post('getdata/{id}', [SiteController::class, "getdataAction"]);
+    Route::get('tafsir/{id}', [SiteController::class, "tafsir"]);
+    Route::get('addlink/{id}', [SiteController::class, "addlink"]);
+    Route::post('addlink/{id}', [SiteController::class, "addlinkAction"])->name("addlinkAction");
+    Route::get('{id}/links/', [SiteController::class, "getlinks"])->name("links");
+    Route::get('edit/{id}', [SiteController::class, "edit"])->name("site.edit");
+    Route::get('delete/link/{id}', [SiteController::class, "deleteLink"])->name("link.delete");
+    Route::get('delete/site/{id}', [SiteController::class, "deleteSite"])->name("site.delete");
+    Route::post('edit/{id}', [SiteController::class, "editAction"])->name("site.edit.action");
+    Route::get('wolt', [SiteController::class, "wolt"]);
+    Route::get('curl', [SiteController::class, "curl"]);
+});
+
+Route::group(['prefix' => 'retail'], function () {
+    Route::get('/', [RetailController::class, "show"])->name("retails");
+    Route::get('/create', [RetailController::class, "create"])->name("retail.create");
+    Route::post('/create', [RetailController::class, "createAction"])->name("retail.create.action");
+    Route::get('/download', [RetailController::class, "download"])->name("retail.download");
+});
+
+Route::get('/derman/export', [DermanController::class, "export"])->name("derman.export");
+Route::get('/selenium', [SeleniumController::class, "scrapePharmastore"])->name("selenium.scrapePharmastore");
